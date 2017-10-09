8ac68a76047229a28abb14cb1ee7d86bb75937c6
kiprok <arthur.vandenbroek@outlook.com>
2017-10-09 14:27:15 +0200

    char can move left right

--- Fight Legends Bang Bang/scripts/Main.js
+++ Fight Legends Bang Bang/scripts/Main.js
@@ -1,13 +1,16 @@
 var container;
 var controls;
 var camera, scene, renderer, interfaceCamera, interfaceScene;
-
+var direction = 0;
+var speed = 8;
+var clock = new THREE.Clock(true);
 'use strict';
 
 Physijs.scripts.worker = 'physi/physijs_worker.js';
 Physijs.scripts.ammo = 'ammo.js';
 
-
+scene = new Physijs.Scene;
+scene.setGravity(new THREE.Vector3( 0,-30,0));
 init();
 
 function init() {
@@ -24,7 +27,7 @@ function init() {
     document.body.appendChild( container );
 	container.appendChild( renderer.domElement );
 		
-	scene = new Physijs.Scene;
+;
 	
 	camera = new THREE.PerspectiveCamera(
 		35,
@@ -54,14 +57,25 @@ function init() {
 
     box = new Physijs.BoxMesh(
 			new THREE.CubeGeometry( 5, 5, 5 ),
-			new THREE.MeshBasicMaterial({ color: 0x888888 })
+			new THREE.MeshBasicMaterial({ color: 0x888888 },
+            1)
 	);
     box.castShadow = true;
-    box.position.set(0,5,0);
+    box.position.set(0,15,0);
 	scene.add( box );
 
+    testbox = new Physijs.BoxMesh(
+			new THREE.CubeGeometry( 5, 5, 5 ),
+			new THREE.MeshBasicMaterial({ color: 0x888888 })
+	);
+    testbox.castShadow = true;
+    testbox.position.set(0,5,-10);
+	scene.add( testbox );
+
+    //console.log(box.getvelocity());
+
     floor = new Physijs.BoxMesh(
-			new THREE.CubeGeometry( 20, 2, 20 ),
+			new THREE.CubeGeometry( 15, 2, 50 ),
 			new THREE.MeshBasicMaterial({ color: 0x48ff00 }),
             0
 	);
@@ -69,6 +83,7 @@ function init() {
     floor.position.set(0,-5,0);
     scene.add(floor);
 
+    scene.simulate();
     requestAnimationFrame( animate );
 
     /*
@@ -97,6 +112,36 @@ function init() {
     */
 }
 
+ window.addEventListener('keydown', function(event) {
+    if (event.keyCode == 65) { //a
+        direction = 1;
+    } else if (event.keyCode == 68) { //d
+        direction = -1;
+    } else if (event.keyCode == 87) { //w
+    } else if (event.keyCode == 83) { //s
+    }
+ });
+
+ window.addEventListener('keyup', function(event) {
+    if (event.keyCode == 65) { //a
+        direction = 0;
+    } else if (event.keyCode == 68) { //d
+        direction = 0;
+    } else if (event.keyCode == 87) { //w
+    } else if (event.keyCode == 83) { //s
+    }
+ });
+
+
+scene.addEventListener( 'update', function() {
+    var timeElapsed = clock.getDelta();
+    // the scene's physics have finished updating
+    box.position.z += (direction *speed)*timeElapsed;
+    box.setAngularFactor( new THREE.Vector3(0,0,0));
+    box.__dirtyPosition = true;
+    scene.simulate(); // run physics
+});
+
 function onWindowResize() {
     windowHalfX = window.innerWidth / 2;
     windowHalfY = window.innerHeight / 2;
@@ -107,7 +152,6 @@ function onWindowResize() {
 }
 
 function animate() {
-    scene.simulate(); // run physics
     render();
     requestAnimationFrame(animate);
 }
