b2938ec238bd8ebe98272a0a47fb2768898160ec
Tijs 1600 <Tijs1600@student.nhl.nl>
2017-10-11 11:56:19 +0200

    Characters added + slow flamingo

---
+++ Fight Legends Bang Bang/scripts/Characters/Jens.js
@@ -0,0 +1,32 @@
+class Jens extends Character{
+    
+    constructor(y, z){
+        super();
+        this.name = "Jens";
+        var material = Physijs.createMaterial(
+        new THREE.MeshBasicMaterial({ color: 0x888888 }),
+        1,
+        1
+    );
+
+        this.specialAtkString = "Cannon Barrage";
+            this.geometry = new Physijs.BoxMesh(
+			new THREE.CubeGeometry( 5, 5, 5 ),
+			material,
+	);
+    this.geometry.castShadow = true;
+    this.geometry.position.set(0,y,z);
+	scene.add( this.geometry );
+    console.log("created " + this.name);
+    }
+
+    block(){
+        //no block
+    }
+
+    specialAtk(){
+        console.log(this.name + " Used " + this.specialAtkString);
+        // Cannon ball!
+    }
+
+}
