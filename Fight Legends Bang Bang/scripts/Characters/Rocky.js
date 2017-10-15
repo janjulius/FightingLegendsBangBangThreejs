9a56387b92d5f5397c6975db05ad7880b4b7357f
JanJulius <lang1600@student.nhl.nl>
2017-10-15 15:56:36 +0200

    help

--- Fight Legends Bang Bang/scripts/Characters/Rocky.js
+++ Fight Legends Bang Bang/scripts/Characters/Rocky.js
@@ -8,7 +8,7 @@ class Rocky extends Character{
         this.specialAtkString = "Shivering Leap";
         this.portrait = 'sprites/Characters/MenuSprites/rocky.png';
         this.Speed = 12;
-        this.specialCounter = 10000;
+        this.specialExists = false;
             this.geometry = new Physijs.BoxMesh(
 			new THREE.CubeGeometry( 5, 5, 5 ),
 			new THREE.MeshBasicMaterial({ color: 0xec00fb },
@@ -23,29 +23,41 @@ class Rocky extends Character{
     }
 
     specialAtk(){
-        if(this.specialCounter >= this.specialCounterThreshHold){
-        var target;
+        if(this.specialReady()){
+        this.target;
         for(var i = 0; i < players.length; i++){
             if(this.id == i){
                 continue;
             }
-            if(target === undefined){
-                target = i;
+            if(this.target === undefined){
+                this.target = i;
             }
             if(distanceBetweenVector3(players[i].geometry.position,this.geometry.position) 
-            <= distanceBetweenVector3(players[target].geometry.position,this.geometry.position)){
-                console.log("ROCKY: target " + players[i].name + " is closer than " + players[target].name + "");
-                target = i;
+            <= distanceBetweenVector3(players[this.target].geometry.position,this.geometry.position)){
+                console.log("ROCKY: target " + players[i].name + " is closer than " + players[this.target].name + "");
+                this.target = i;
             }
         }
-        console.log(target);
-       // this.geometry.position.set(target.geometry.position.x, target.geometry.position.y+2, target.geometry.position.z-2);
-        players[target].setDamage(players[target].getDamage() + 50);
-        console.log("Trying to go from :" + this.geometry.position.y + ", " + this.geometry.position.z +" to:" + players[target].geometry.position.y + "," +players[target].geometry.position.z);
-        this.geometry.position.set(players[target].geometry.position.x,players[target].geometry.position.y,players[target].geometry.position.z);
-        console.log("new " +this.geometry.position.y + ", " + this.geometry.position.z);
-        //this.setSpecialAttackCounter(this.getSpecialAttackCounter - this.specialCounterThreshHold);
+        this.specialExists = true;
+        this.specialTimer = 1;
+        }
     }
+
+    UpdateChar(t){
+        if(this.specialExists){
+            if(this.specialTimer > 0){
+                console.log("test" + this.specialTimer);
+                this.specialTimer -= t;
+            }
+            if(this.specialTimer <= 0){
+                console.log("ATTACK");
+                this.specialExists = false;
+                players[this.target].setDamage(players[this.target].getDamage() + 50);
+                this.geometry.position.set(players[this.target].geometry.position.x,players[this.target].geometry.position.y,players[this.target].geometry.position.z);
+                
+            }
+        }
+        
     }
 
 }
