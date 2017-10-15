bc6b059e6002597d14ffc8ff1807eb3367cdecba
JanJulius <lang1600@student.nhl.nl>
2017-10-15 18:08:33 +0200

    halve paardman special

--- Fight Legends Bang Bang/scripts/Characters/Paardman.js
+++ Fight Legends Bang Bang/scripts/Characters/Paardman.js
@@ -6,14 +6,19 @@ class Paardman extends Character {
         this.cid = 2;
         this.specialAtkString = "Stone, paper, scissors";
         this.portrait = 'sprites/Characters/MenuSprites/paardman2.png';
+        this.stoneThrowSpeed = 40;
+        this.scissorThrowSpeed = 50;
+        this.paperThrowSpeed = 1;
+        this.target;
+        this.stoneDamage = 60;
+        this.scissorDamage = 80;
+        this.paperDamage = 250;
+        if (DEBUG_MODE) { this.specialIncrease = 100; }
         this.geometry = new Physijs.BoxMesh(
             new THREE.CubeGeometry(5, 5, 5),
             new THREE.MeshBasicMaterial({ color: 0xffad60 },
                 1)
         );
-
-        let horseChoice; //1 = stone, 2 = paper, 3 = scissors
-        let horseChoiceString;
 
         this.geometry.castShadow = true;
         this.geometry.position.set(0, y, z);
@@ -22,24 +27,107 @@ class Paardman extends Character {
     }
 
     specialAtk() {
-        console.log(this.name + " special attack");
 
-        // battle cry animation 
+        if (this.specialObject != undefined) { scene.remove(this.specialObject); }
+        this.horseChoice = 2;//Math.floor((Math.random() * 3) + 1);
+        switch (this.horseChoice) {
+            case 1:   //stone     
+                var stoneMaterial = Physijs.createMaterial(
+                    new THREE.MeshBasicMaterial({
+                        color: 0x4d4c4c
+                    }),
+                    1,
+                    0
+                );
 
-        horseChoice = Math.floor((Math.random() * 3) + 1);
-        switch (horseChoice) {
-            case 1:
-                horseChoiceString = "Stone";
-                //play animation steen
+                this.specialObject = new Physijs.BoxMesh(
+                    new THREE.CubeGeometry(4, 4, 4),
+                    stoneMaterial
+                );
+                this.specialExists = true;
+                this.specialTimer = 2;
+                this.specialObject.position.set(0, this.geometry.position.y, this.geometry.position.z + (5 * this.attackDirection));
+
+                this.stoneVelocity = new THREE.Vector3(0, 0, this.attackDirection * ((this.stoneThrowSpeed) + 1));
+                scene.add(this.specialObject);
                 break;
-            case 2:
-                horseChoiceString = "Paper";
-                //play animation paper
+            case 2: //paper
+            var stoneMaterial = Physijs.createMaterial(
+                new THREE.MeshBasicMaterial({
+                    color: 0xffffff
+                }),
+                1,
+                0
+            );
+
+            this.specialObject = new Physijs.BoxMesh(
+                new THREE.CubeGeometry(2, 0.1, 1),
+                stoneMaterial
+            );
+                this.specialExists = true;
+                this.specialTimer = 300;
+                this.specialObject.position.set(0, this.geometry.position.y, this.attackDirection *(this.geometry.position.z + 7.5));
+
+                scene.add(this.specialObject);
+
                 break;
-            case 3:
-                horseChoiceString = "Scissors";
-                //play animation scissors
+            case 3: //scissors
+                //horseChoiceString = "Scissors";
+
                 break;
+        }
+    }
+
+    UpdateChar(t) {
+        if (this.specialExists) {
+            if (this.specialTimer >= 0) {
+                this.specialTimer -= t;
+                switch (this.horseChoice) {
+                    case 1:
+                        this.specialObject.setLinearVelocity(this.stoneVelocity);
+
+                        var _this = this;
+                        this.specialObject.addEventListener('collision', function (other_object, relative_velocity, relative_rotation, contact_normal) {
+                            if (_this.specialObject._physijs.touches.length > 0) {
+                                if (other_object.isPlayer) {
+                                    _this.target = parseInt(other_object.name);
+                                    if (_this.id != _this.target) {
+                                        _this.specialTimer = -1;
+                                    }
+                                }
+                            }
+                        });
+                        break;
+
+                    case 2:
+                        this.specialObject.position.y -= .5;
+                        var _this = this;
+                        this.specialObject.addEventListener('collision', function (other_object, relative_velocity, relative_rotation, contact_normal) {
+                            if (_this.specialObject._physijs.touches.length > 0) {
+                                if (other_object.isPlayer) {
+                                    _this.target = parseInt(other_object.name);
+                                    if (_this.id != _this.target) {
+                                        _this.specialTimer = -1;
+                                    }
+                                }
+                            }
+                        });
+                        break;
+                    case 3:
+
+                        break;
+                }
+                console.log(this.specialTimer);
+            }
+            if (this.specialTimer <= 0) {
+                console.log("remove" + this.target);
+                this.specialExists = false;
+                scene.remove(this.specialObject);
+                this.setSpecialAttackCounter(this.specialCounter + this.specialIncrease);
+                if (this.target != undefined) {
+                    players[this.target].setDamage(players[this.target].getDamage() + this.paperDamage, this.attackDirection);
+                }
+            }
         }
     }
 
