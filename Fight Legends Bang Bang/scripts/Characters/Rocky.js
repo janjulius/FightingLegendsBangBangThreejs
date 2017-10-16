d11dd90a39c6a8a9e9a7c75004f325f3893af8e4
JanJulius <lang1600@student.nhl.nl>
2017-10-16 23:48:05 +0200

    rocky nerf + sound

--- Fight Legends Bang Bang/scripts/Characters/Rocky.js
+++ Fight Legends Bang Bang/scripts/Characters/Rocky.js
@@ -14,7 +14,11 @@ class Rocky extends Character {
         this.clawed = [];
         this.clawed[0] = false; this.clawed[1] = false; this.clawed[2] = false;
 
-        this.ultVanishAudio = new Audio('Sounds/Characters/Rocky/Rocky_ult_vanish.wav');
+        var soundFolderPath = 'Sounds/Characters/Rocky/';
+        this.ultVanishAudio = new Audio(soundFolderPath + 'Rocky_ult_vanish.wav');
+        this.claw1HitAudio = new Audio(soundFolderPath + 'Rocky_Claw_hit.wav');
+        this.claw2HitAudio = new Audio(soundFolderPath + 'Rocky_Claw_hit.wav');
+        this.claw3HitAudio = new Audio(soundFolderPath + 'Rocky_Claw_hit.wav');
         this.geometry = new Physijs.BoxMesh(
             new THREE.CubeGeometry(5, 5, 5),
             new THREE.MeshBasicMaterial({ color: 0xec00fb },
@@ -59,37 +63,40 @@ class Rocky extends Character {
             if (this.specialTimer > 0) {
                 console.log("test" + this.specialTimer);
                 this.specialTimer -= t;
-                if (this.specialTimer > 1.50 && this.specialTimer < 2) { //preparing
+                if (this.specialTimer > 2 && this.specialTimer < 2.5) { //preparing
                     this.isStunned = true;
                 }
-                if (this.specialTimer > 1.25 && this.specialTimer < 1.50) { //jump
+                if (this.specialTimer > 1.75 && this.specialTimer < 2) { //jump
                     this.ultVanishAudio.play();
                     this.isStunned = false;
                     this.spJumpVelocity = new THREE.Vector3(0, 20, this.attackDirection.z * ((40) + 1));
                     this.geometry.setLinearVelocity(this.spJumpVelocity);
                 }
-                if (this.specialTimer > 0.5 && this.specialTimer < 1.25) { //dissapear
+                if (this.specialTimer > 1 && this.specialTimer < 1.75) { //dissapear
                     this.isStunned = true;
                     this.geometry.setLinearVelocity(new THREE.Vector3(0, 0, 0));
                     this.geometry.position.x = -10000;
                 }
-                if (this.specialTimer < 0.5) {
+                if (this.specialTimer < 1) {
                     this.geometry.position.set(players[this.target].geometry.position.x, players[this.target].geometry.position.y,
                         players[this.target].geometry.position.z + -players[this.target].attackDirection.z * 5);
                     players[this.target].isStunned = true;
                     if (!this.clawed[0]) {
                         players[this.target].setDamage(players[this.target].getDamage() + this.clawDamage, { y: 0, z: 0 });
                         this.clawed[0] = true;
+                        this.claw1HitAudio.play();
                     }
-                    if (this.specialTimer < 0.375) {
+                    if (this.specialTimer < 0.625) {
                         if (!this.clawed[1]) {
                             players[this.target].setDamage(players[this.target].getDamage() + this.clawDamage, { y: 0, z: 0 });
                             this.clawed[1] = true;
+                            this.claw2HitAudio.play();
                         }
-                        if (this.specialTimer < 0.175) {
+                        if (this.specialTimer < 0.3) {
                             if (!this.clawed[2]) {
                                 players[this.target].setDamage(players[this.target].getDamage() + this.clawDamage, { y: 0, z: 0 });
                                 this.clawed[2] = true;
+                                this.claw3HitAudio.play();
                             }
                         }
                     }
