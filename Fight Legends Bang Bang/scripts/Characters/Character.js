fe7c0d2bacda39ae1c332475b389bc6e96e2352f
JanJulius <lang1600@student.nhl.nl>
2017-10-18 17:58:56 +0200

    crowd sound, jesns sound en boomstronk sound

--- Fight Legends Bang Bang/scripts/Characters/Character.js
+++ Fight Legends Bang Bang/scripts/Characters/Character.js
@@ -44,7 +44,7 @@ class Character {
         this.canBlock = true;
 
         this.basicAttackDamage = 10;
-        this.specialIncrease = 10;
+        this.specialIncrease = 100;
 
         this.swingTimer = 0;
         this.attackRemoveTimer = 0;
@@ -64,6 +64,8 @@ class Character {
         this.gravityVelocity = 80;
 
         this.touchingWalls = [-1, -1, -1, -1];
+
+
         
         this.blockMaterial = THREE.ImageUtils.loadTexture('sprites/Characters/BlockIcon.png');
         this.blockObject = new THREE.Mesh(
@@ -158,7 +160,6 @@ class Character {
         this.canBlock = false;
         this.blockTimer = this.blockTime;
         this.blockCooldownTimer = this.blockCooldownTime;
-        
     }
 
     idle() {
@@ -189,9 +190,8 @@ class Character {
     }
 
     setDamage(d, dir) {
-        if (this.blocking) {
-            return;
-        }
+        if (!this.blocking) {
+        
 
         if (this.CheckSides("down"))
             dir.y = Math.abs(dir.y);
@@ -208,6 +208,7 @@ class Character {
             this.knockBack.y = ((d + 20) * this.damageMulti) * dir.y;
         }
         gameInterface.UpdateGameInterface(this.id);
+    }
     }
 
     getSpecialAttackCounter() {
@@ -240,6 +241,12 @@ class Character {
         };
 
         if (!arena.Contains(playerPoint) && this.isAlive) {
+            var r = Math.floor((Math.random() * 3));
+            var cheerSounds = ['Sounds/Crowd/Crowd_cheer_1.wav', 'Sounds/Crowd/Crowd_cheer_2.wav', 'Sounds/Crowd/Crowd_cheer_3.wav'];
+            var cSound = new Audio(cheerSounds[r]);
+            cSound.volume = MUSIC_VOLUME;
+            cSound.play();
+            console.log(r);
             this.respawnTimer = this.respawnDelay;
             this.isAlive = false;
             this.isStunned = true;
@@ -478,7 +485,6 @@ class Character {
         if(this.blockCooldownTimer <= 0)
             this.canBlock = true;
         
-        console.log(this.blockCooldownTimer);
     }
 
     UpdateChar(t) {}
