01b1d216f1fbc72b8768f0dfefff97c83a6a3ef9
Tijs 1600 <Tijs1600@student.nhl.nl>
2017-10-19 13:17:28 +0200

    Test PretPaleis maar even :)

--- Fight Legends Bang Bang/scripts/Main.js
+++ Fight Legends Bang Bang/scripts/Main.js
@@ -304,7 +304,7 @@ function runGame() {
             charScreens[i].position.set(100, 100, 100);
         }
 
-        level = new HyruleCastle(); //temp level changer
+        level = new PretPaleis(); //temp level changer
 
         /*
         //level randomizer
