7b89adb0830952e621948260a15740f517bad692
kiprok <arthur.vandenbroek@outlook.com>
2017-10-31 00:29:09 +0100

    kees model

--- Fight Legends Bang Bang/scripts/Characters/Kees.js
+++ Fight Legends Bang Bang/scripts/Characters/Kees.js
@@ -3,6 +3,8 @@ class Kees extends Character {
     constructor(y, z) {
         super();
         this.name = "Kees";
+        this.size.width = 5;
+        this.size.height = 5;
         this.cid = 4;
         this.portrait = 'sprites/Characters/MenuSprites/kees.png';
         this.hitplayer = [false, false, false, false];
@@ -17,14 +19,59 @@ class Kees extends Character {
         );
 
         this.specialAtkString = "Expose to the light";
+        this.modelOfset = new THREE.Vector3(0, -this.size.height / 2, 0);
         this.geometry = new Physijs.BoxMesh(
-            new THREE.CubeGeometry(5, 5, 5),
+            new THREE.CubeGeometry(5, this.size.height, this.size.width),
             material,
         );
         this.geometry.castShadow = true;
         this.geometry.position.set(0, y, z);
         scene.add(this.geometry);
+        this.loadModel();
         console.log("created " + this.name);
+    }
+
+    loadModel() {
+        console.log("loading model...");
+        var manager = new THREE.LoadingManager();
+        manager.onProgress = function (item, loaded, total) {
+            console.log(item, loaded, total);
+        };
+
+        var onProgress = function (xhr) {
+            if (xhr.lengthComputable) {
+                var percentComplete = xhr.loaded / xhr.total * 100;
+                console.log(Math.round(percentComplete, 2) + '% downloaded');
+            }
+        };
+        var onError = function (xhr) {
+            console.log(xhr);
+        };
+
+        THREE.Loader.Handlers.add(/\.dds$/i, new THREE.DDSLoader());
+
+        var mtlLoader = new THREE.MTLLoader();
+
+        var _this = this;
+        var loader = new THREE.FBXLoader(manager);
+
+        loader.load('Models/kees/Idle.fbx', function (object) {
+            object.scale.set(1, 1, 1);
+            _this.pivot.add(object);
+            _this.geometry.add(_this.pivot);
+            object.position.set(_this.modelOfset.x, _this.modelOfset.y, _this.modelOfset.z);
+            _this.model = object;
+            object.mixer = new THREE.AnimationMixer(object);
+            mixers.push(object.mixer);
+
+            _this.idleAnim = object.mixer.clipAction(object.animations[0]);
+            _this.idleAnim.play();
+            
+            loader.load('Models/kees/Running.fbx', function (object) {
+                _this.runningAnim = _this.model.mixer.clipAction(object.animations[0]);
+            }, onProgress, onError);
+
+        }, onProgress, onError);
     }
 
 
